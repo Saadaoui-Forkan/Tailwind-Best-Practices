981f213c0f770a113b24500b7b73d6c2a2a08012
unknown <forkan.saadaoui@gmail.com>
2023-10-16 23:16:05 +0100

    using @apply

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@
 function App() {
   return (
     <div >
-      Hello World
+      <h1 className="text-3xl mb-4 text-center">Welcome</h1>
+      <p className="text-center text-sm text-slate">Start building your Tailwind project</p>
+      <div className="div">
+        <button className="btn">click me</button>
+      </div>
     </div>
   );
 }
